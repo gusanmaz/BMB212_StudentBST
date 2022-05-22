7faa4151e4c172dbdab08694ef426d92fe69b66c
Güvenç Usanmaz <gusanmaz@gmail.com>
2022-05-22 12:32:23 +0300

    Create BSTNode.java

---
+++ BSTNode.java
@@ -0,0 +1,14 @@
+public class BSTNode {
+    public Student data;
+    public BSTNode left;
+    public BSTNode right;
+
+    public BSTNode(){
+    }
+
+    public BSTNode(Student s){
+        data   = s;
+        left  = null;
+        right = null;
+    }
+}

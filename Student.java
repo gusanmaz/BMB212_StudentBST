d8f6361e9f5cd01e89d1881afa632fbb8ed484a9
Güvenç Usanmaz <gusanmaz@gmail.com>
2022-05-22 12:31:51 +0300

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,35 @@
+public class Student {
+    private int ID;
+    private String name;
+    private String surname;
+    private double GPA;
+
+    public Student(int ID, String name, String surname, double GPA){
+        this.ID = ID;
+        this.name = name;
+        this.surname = surname;
+        this.GPA = GPA;
+    }
+
+    // Bu metod BST sinifindaki toDOT metodunda kullanilmaktadir.
+    
+    public String ToDotString(){
+        return "\"" + ID + "\\n" + name + " " + surname + "\\n" + GPA + "\"";
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getSurname(){
+        return surname;
+    }
+
+    public double getGPA(){
+        return GPA;
+    }
+}
